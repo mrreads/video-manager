fefae91cbd8d5f9061eae39ead6b390a86e9e18d
Alexander <mrreads@yandex.com>
2021-05-08 23:56:09 +0300

    added translation changer

--- src/views/Setting.vue
+++ src/views/Setting.vue
@@ -1,9 +1,22 @@
 <template>
   <br />
   <el-divider content-position="left"> <h1>{{ t('setting.title') }}</h1> </el-divider>
-
   <el-main>
-    <p>test</p>
+    
+    <div class="setting">
+      <p> {{ t('setting.select_lang') }}: </p>
+      <el-select v-model="currentLocale" :placeholder="t('setting.select_lang')" @change="changeLang">
+        <el-option
+          v-for="lang in locales"
+          :key="lang.value"
+          :label="lang.label"
+          :value="lang.value">
+          <span style="float: left">{{ lang.label }}</span>
+          <span style="float: right; color: #8492a6; font-size: 13px">{{ lang.value }}</span>
+        </el-option>
+      </el-select>
+    </div>
+
   </el-main>
 </template>
 
@@ -11,11 +24,31 @@
 import { useI18n } from "vue-i18n";
 export default
 {
-  name: "Setting",
+  name: "Settings",
+
+  data() {
+    return {
+      locales: [
+        {
+          value: 'en',
+          label: 'English'
+        }, {
+          value: 'ru',
+          label: 'Russian'
+        }],
+        currentLocale: this.$i18n.locale
+    }
+  },
 
   setup() {
     const { t } = useI18n();
     return { t };
+  },
+
+  methods: {
+    changeLang: function() {
+      this.$i18n.locale = this.currentLocale;
+    }
   }
 }
 </script>
